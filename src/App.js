161f2fb55cf22acb3a7502a7b497a1f197c6aa8b
Rezaul909 <reza1381996@gmail.com>
2022-03-26 20:58:20 +0600

    Ques-added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Shop from './Components/Shop/Shop';
+import QuesAns from './Components/QuesAns/QuesAns';
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -18,6 +19,7 @@ function App() {
     <div className="App container">
       <h1 className='text-primary my-5'>Samsung Smart Shop</h1>
       <Shop products= {products} ></Shop>
+      <QuesAns></QuesAns>
     </div>
   );
 }
